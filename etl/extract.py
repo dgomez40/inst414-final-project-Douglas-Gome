d39fe6cd7c8cb174950f954b70b45bacc6d5c1c3
lexmd28 <163152659+dgomez40@users.noreply.github.com>
2025-08-14 22:42:27 -0400

    part 3 final project
    I have to wake up really early, so I am hoping I can resubmit but I think I have made a fat chunk of progress so far!

--- etl/extract.py
+++ etl/extract.py
@@ -6,25 +6,43 @@ import numpy as np
 
 def air_pollutant():
 
-    pm25_raw = pd.read_csv('data/pm25.csv')
-    print(pm25_raw)
+    pm25_raw = pd.read_csv(
+        "data/pm25.csv",
+        parse_dates=['DATE'],
+        date_format= '%m/%d/%y'
+
+    )
+    pm25_raw.to_csv("data/pm25_raw.csv")
+    return pm25_raw
+
 
 def weather():
 
-    temperature_raw = pd.read_csv('data/temperature.csv')
-    print(temperature_raw)
+    temperature_raw = pd.read_csv(
+    'data/temperature.csv',
+    parse_dates=['DATE'],
+    date_format='%m/%d/%y'
+)
+    # print(temperature_raw)
+    temperature_raw.to_csv('data/temperature_raw.csv')
+    return temperature_raw
 
-#my next steps would be to get access to proper temperature csv file, current one wouldn't work that well right now
-#then I could merge the two databases off a common key, date, I have to find temp file that does not go through the entire day
-#but the data provider is down at the moment https://www.ncei.noaa.gov/access/search/index
-#if this dataset doesn't come up I have been looking into other resources.
-
-#sort the data from oldest data to newest perferrably just 2025 data would be fine, plenty of rows,
-#this will be my training data
 
 def merge():
-    #merge two datasets
+    # Load datasets
+    temperature_raw = weather()
+    pm25_raw = air_pollutant()
+    
+    # Find the latest date in the smaller dataset
+    max_pm25_date = pm25_raw['DATE'].max()
+    # print(max_pm25_date)
 
+    # # Filter temperature_raw so it only goes up to the max date (Oct 31 in this case)
+    temperature_filtered = temperature_raw[temperature_raw['DATE'] <= max_pm25_date]
+    # print(temperature_filtered)
 
+    # # Merge on the date column
+    pollutant_merge = pd.merge(temperature_filtered, pm25_raw, on='DATE')
+    pollutant_merge.to_csv('data/pollutant_merge.csv')
 
-    return pollutant_merge;
+    return pollutant_merge
--- main.py
+++ main.py
@@ -3,17 +3,17 @@ import etl.extract as extract
 import etl.transform as transform
 import etl.load as load
 def main():
-        extract.air_pollutant()
-        extract.weather()
+        # extract.air_pollutant()
+        # extract.weather()
         extract.merge()
+        
+        transform.drop_columns()
+        transform.outliers()
+        transform.split_into_train_and_test()
+        model, X_train_scaled, X_test_scaled, y_train, y_test, y_pred = transform.train_lin_reg()
 
-        transform.unique_id(pollutant_merge)
-        transform.missing_value(pollutant_merge)
-        transform.outliers(pollutant_merge)
-        transform.normalization(pollutant_merge)
-
-        load.save_transformed_csv(pollutant_merge)
-
+        load.load_data(X_train_scaled, X_test_scaled, y_train, y_test)
+        
 
 
         
